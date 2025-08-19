7f8bceb7cab7e5f7708ac4d1cde7f6816a6af3fe
BitByte08 <me@bitworkspace.kr>
2025-08-19 09:14:52 +0900

    chore :: 프로젝트에 맞춰 이름 변경

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ PORT = 1883
 
 def on_connect(client, userdata, flags, rc, properties=None):
     print("Connected with result code", rc, flush=True)
-    client.subscribe("test/topic")
+    client.subscribe("helper/#")
 
 def on_message(client, userdata, msg):
     print(f"{msg.topic} {msg.payload.decode()}", flush=True)
